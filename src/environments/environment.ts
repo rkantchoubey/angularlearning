184769abc9b524baf77009cd33583090173562b5
rkantchoubey <rkant.choubey@gmail.com>
2020-01-19 22:05:46 +0530

    fixing the conflicts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,5 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-<<<<<<< HEAD
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-=======
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
->>>>>>> f987489254a231685d1e8f7f895d2478f7782125
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
@@ -11,17 +7,9 @@ export const environment = {
 };
 
 /*
-<<<<<<< HEAD
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
  * import the following file, but please comment it out in production mode
  * because it will have performance impact when throw error
-=======
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
->>>>>>> f987489254a231685d1e8f7f895d2478f7782125
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
